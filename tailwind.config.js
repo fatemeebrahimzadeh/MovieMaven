596a85ad3129260c091a26468a58275dbc3d1d91
fateme ebrahimzadeh <fateme.ebrahimzadeh20@gmail.com>
2023-05-04 16:24:55 +0330

    chore(app): change tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: "10rem",
+    },
+    extend: {
+      colors: {
+        light: {
+          // for sample
+          // 100: '#F2F9FF',
+        },
+        dark: {
+          // for sample
+          // 100: '#F8FAFB',
+        },
+      },
+      fontFamily: {
+        dosis: ["Dosis", "sans-serif"],
+      },
+      backgroundImage: {
+        header: "url('/assets/images/header-bg.jpg')",
+      },
+    },
   },
   plugins: [],
-}
-
+};
