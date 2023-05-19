f9e824c901d661031532c962e2596f31db46287f
fateme ebrahimzadeh <fateme.ebrahimzadeh20@gmail.com>
2023-05-19 21:18:47 +0330

    style(app): change global padding for being more responsible

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
   theme: {
     container: {
       center: true,
-      padding: "5rem",
+      padding: {
+        DEFAULT: "2rem",
+        lg: "5rem",
+        "2xl": "15rem",
+      },
     },
     extend: {
       boxShadow: {
